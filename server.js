c2b80397fc8d97ccf74b54c7866e8256a0852051
Corey B <corey.bennett@live.com>
2022-09-21 19:10:01 -0400

    HTML display and app.use entries

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const path = require('path');
 const fs = require('fs');
 
 // Call upon unique id generator
 const uuid = require('./uuid');
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Main HTML displayed
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
