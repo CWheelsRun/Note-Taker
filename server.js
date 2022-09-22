b73270499fcfdceb32fa01811565c2358739cf4d
Corey B <corey.bennett@live.com>
2022-09-21 21:11:48 -0400

    code consolidated, testing successful id generation

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-// Call upon unique id generator
-const uuid = require("./uuid");
-
 const PORT = process.env.PORT || 3001;
 
 const app = express();
